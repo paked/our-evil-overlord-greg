97f4724e73de6e8e5f7b095b567fa0d0a8ed356d
Harrison <harrison@theshoebridges.com>
2015-04-18 13:08:57 +1000

    +assets/, js/game: add map and basic tileset

--- js/game.js
+++ js/game.js
@@ -1,19 +1,31 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', {preload: preload, create: create, update: update});
 
+var map;
+var platforms;
+var ladders;
+
 function preload() {
     // preload content
+    game.load.image('tilesheet', 'assets/tileset.png');
+    game.load.tilemap('map', 'assets/maps/level.json', null, Phaser.Tilemap.TILED_JSON);
 }
 
 function create() {
     // create entities
     game.stage.backgroundColor = '#fff';
+    game.camera.scale.x = 2;
+    game.camera.scale.y = 2;
 
     var text = game.add.text(16, 16, 'GREG IS HAPPY', {fontSize:'32px', fill: '#000'});
     text.anchor.set(0.5);
     text.x = game.width / 2;
     text.y = game.height / 4;
-}
 
+    map = game.add.tilemap('map');
+    map.addTilesetImage('tileset', 'tilesheet');
+    platforms = map.createLayer('Platforms');
+    ladders = map.createLayer('Ladders');
+}
 
 function update() {
     // update sprites
